2a7e851f7414cb23281e04cef1dd286c0387316e
Vinicius Dias <diasvinicius95@outlook.com>
2023-05-05 17:15:27 -0300

    atualização estrutural e atualizando quantidades na lista!

--- js/main.js
+++ js/main.js
@@ -1,49 +1,58 @@
-const form = document.getElementById("novoItem")
+// Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem, modificando o valor de `string` com JSON.parse()
+const form = document.getElementById("novoItem") 
 const lista = document.getElementById("lista")
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []       
 
-form.addEventListener("submit", (evento)=>{
-    evento.preventDefault()
+// Refatoração do addEventListener para receber as funções extras da função criaElemento
+form.addEventListener("submit", (evento) => {   
+    evento.preventDefault()            
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
+    const itemAtual = {
+    "nome": nome.value,
+    "quantidade": quantidade.value
+    }
+
+    const existe = itens.find(elemento => elemento.nome === nome.value)
     
+    if(existe){
+        itemAtual.id = existe.id
+        atualizaElemento(itemAtual)
+    }else{
+        itemAtual.id = itens.length
+        criaElemento(itemAtual)
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
     nome.value = ""
     quantidade.value = ""
-   
+
 })
 
-function criaElemento(objeto, quantidade){
+// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página 
+itens.forEach( (elemento) => {    
+    criaElemento(elemento)
+} ) 
 
-    const novoItem = document.createElement("li")
+// Refatoração da função `criaElemento` para que possua apenas a função que faça sentido ao nome. 
+function criaElemento(item) {  
+    const novoItem = document.createElement('li')
     novoItem.classList.add("item")
     novoItem.classList.add("shadow")
 
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
 
-    const numeroDeItens = document.createElement("strong")
-    numeroDeItens.innerHTML = quantidade
-    novoItem.appendChild(numeroDeItens)
-    
-    novoItem.innerHTML += objeto
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item.nome
     lista.appendChild(novoItem)
-
-    const itemAtual = {
-        "nome" : objeto,
-        "quantidade": quantidade
-    }
-
-    itens.push(itemAtual)
-
-    localStorage.setItem("itens", JSON.stringify(itens))
 }
 
-// Verifica se há dados no Local Storage
-if(localStorage.getItem('itens')) {
-    // Se houver dados, adiciona-os à página
-    var item =JSON.parse(localStorage.getItem('itens'));
-    for(var i = 0; i<item.length; i++){
-        criaElemento(item[i].nome, item[i].quantidade)
-    }
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
